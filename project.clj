29939b5c55e93919b77702f2e75d14df966bf498
Eero Helenius <eero.helenius@citec.com>
2015-02-12 22:02:18 +0200

    Add missing xml-resolver dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-saxon "0.9.4"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [net.sf.saxon/Saxon-HE "9.6.0-2"]])
+                           [net.sf.saxon/Saxon-HE "9.6.0-2"]
+                           [xml-resolver/xml-resolver "1.2"]])
