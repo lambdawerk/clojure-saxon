4e0f5c458b61072e977b4e7bd21031376bcab31c
Eero Helenius <eero.helenius@citec.com>
2015-11-20 19:15:38 +0200

    Update Saxon version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-saxon "0.10.0"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :dependencies [[org.clojure/clojure "1.7.0"]
-                           [net.sf.saxon/Saxon-HE "9.6.0-2"]
+                           [net.sf.saxon/Saxon-HE "9.6.0-7"]
                            [xml-resolver/xml-resolver "1.2"]])
