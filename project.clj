f966ab67a1a39e32511091d33d56b4f7df1246ed
Hans Huebner <hans.huebner@lambdawerk.com>
2016-09-29 13:15:01 +0200

    Update project name in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-saxon "0.10.0"
+(defproject lambdawerk/clojure-saxon "0.10.0"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :dependencies [[org.clojure/clojure "1.7.0"]
                            [net.sf.saxon/Saxon-HE "9.6.0-7"]
