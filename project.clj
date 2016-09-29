71c5a80f4958cdd7eb7e0a4f9a66241e1dcf67aa
Hans Huebner <hans.huebner@lambdawerk.com>
2016-09-29 13:24:07 +0200

    Bump version number to 0.11.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdawerk/clojure-saxon "0.10.0-SNAPSHOT"
+(defproject lambdawerk/clojure-saxon "0.11.0-SNAPSHOT"
   :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.sf.saxon/Saxon-HE "9.6.0-7"]
