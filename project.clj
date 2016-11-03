a7a6f8e8387305f2d184bd729fa304d8c6538c85
Hans Huebner <hans.huebner@lambdawerk.com>
2016-11-03 09:40:54 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lambdawerk/clojure-saxon "0.11.0-SNAPSHOT"
   :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.sf.saxon/Saxon-HE "9.6.0-7"]
+                 [lambdawerk/Saxon-EE "9.7.0-10J"]
                  [xml-resolver/xml-resolver "1.2"]])
